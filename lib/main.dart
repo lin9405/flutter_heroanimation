2f11c74c5d60c44a44c8972acd90d7f95583c22a
parkchaerin <21300322@handong.edu>
2019-01-03 01:30:20 +0900

    first

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'second_screen.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Hero Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Flutter Home Page'),
+      routes: <String, WidgetBuilder>{
+        '/second' : (BuildContext context) => SecondScreen()
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  /// Called when the bottom icon is clicked
+  ///
+  /// Navigate to the second screen
+  void _nextScreen(){
+    Navigator.of(context).pushNamed('/second');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      color: Colors.white,
+      child: Column(
+        children: <Widget>[
+          Spacer(),
+          Hero(
+            tag: 'logo',
+            child: Image.asset('assets/llama.png'),
+          ),
+          Spacer(),
+          MaterialButton(child: Text('Next Screen'),
+            textColor: Colors.white,
+            color: Colors.blue,
+            minWidth: 200.0,
+            onPressed: _nextScreen,
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/second_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatefulWidget{
+
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white30,
+        elevation: 0.0,
+        title: Text('Hero Page 2',
+          style: TextStyle(
+              color: Colors.black
+          ),),
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(left: 32.0, top: 16.0),
+        child: Hero(
+          tag: 'logo',
+          child: Image.asset('assets/llama.png',
+            width: 50.0,
+            height: 50.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
